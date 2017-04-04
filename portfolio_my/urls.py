00447e1059fe9c01636bf51eb6a62fcdce63ad70
cjahangir <jahangir.cse09@gmail.com>
2017-04-04 19:24:47 +0600

    added birthday gift

--- portfolio_my/urls.py
+++ portfolio_my/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^/?$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^kakoli/?$', TemplateView.as_view(template_name='birthday.html'), name='birthday'),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
--- portfolio_my/views.py
+++ portfolio_my/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import get_object_or_404, render
 
 def detail(request):
     return render(request, 'index.html')
+
+def birthday(request):
+    return render(request, 'birthday.html')
